0335d3fe4bcab1d52f071baca906cf9a305a9aba
Hector Bonilla <hbonillam95@gmail.com>
2021-11-02 16:55:41 -0600

    useHistory substitutes link

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import pageLogo from "../assets/images/pngwing.com.png";
 import lupaLogo from "../assets/logos/lupa.png";
 import shoppingCartLogo from "../assets/images/shopping-cart.png";
+import { useHistory } from "react-router-dom";
 
 function PageHeader() {
+  const history = useHistory();
   const [searchTerm, setSearchTerm] = useState("");
   const textInputFunction = function (value) {
     const text = value.target.value.toLowerCase().trim();
     setSearchTerm(text);
   };
+  function goHome() {
+    history.push("/home");
+  }
   return (
     <div className="pageHeader">
       <div className="logoContainer">
-        <a href="/home">
-          <img className="logo--md" src={pageLogo} alt="pageLogo" />
-        </a>
+        <img
+          className="logo--md"
+          src={pageLogo}
+          alt="pageLogo"
+          onClick={goHome}
+        />
       </div>
       <div className="top-menu">
         <div className="menu-elements">
