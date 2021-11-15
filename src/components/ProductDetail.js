399f69112d70f6b6bc7c9ef6ef183e0979a556a5
Hector Bonilla <hbonillam95@gmail.com>
2021-11-14 23:42:53 -0600

    Testing

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState, useContext } from "react";
+import Context from "../store/context";
+import { useParams } from "react-router-dom";
+import { useProduct } from "../utils/hooks/useProduct";
+import ImageSwiper from "./ImageSwiper.jsx";
+
+function ProductDetail() {
+  const { state, actions } = useContext(Context);
+  const { productId } = useParams();
+  const response = useProduct(productId);
+  const [product, setProduct] = useState(null);
+  const [cantidad, setCantidad] = useState(1);
+  useEffect(() => {
+    if (!response.isLoading && response?.data?.results?.length > 0) {
+      setProduct(response.data.results[0]);
+    }
+  }, [response]);
+  useEffect(() => {
+    const listaItem = state.value?.filter(
+      (item) => item.producto.id === product?.id
+    );
+    if (listaItem.length > 0) {
+      setCantidad(parseInt(listaItem[0].cantidad));
+    }
+  }, [product, state.value]);
+  const addProductToCart = function (product) {
+    //console.log("anadiendo");
+    actions({ type: "addProduct", payload: { ...state, value: product } });
+  };
+  return (
+    <div className="product-detail">
+      {product && (
+        <div>
+          <div className="swiper-container">
+            <ImageSwiper images={product.data.images}></ImageSwiper>
+          </div>
+          <table className="details-table">
+            <thead>
+              <tr>
+                <th colSpan="2">Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <b>Nombre: </b>
+                </td>
+                <td>
+                  <span>{product.data.name}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Precio: </b>
+                </td>
+                <td>
+                  <span>
+                    $
+                    {product.data.price.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                    })}
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>SKU: </b>
+                </td>
+                <td>
+                  <span>{product.data.sku}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Category: </b>
+                </td>
+                <td>
+                  <span>{product.data.category.slug}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Tags: </b>
+                </td>
+                <td>
+                  <ul>
+                    {product.tags.map((tag, indice) => (
+                      <li key={indice}>
+                        <span>{tag}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Description:</b>
+                </td>
+                <td>
+                  <p>
+                    {product.data.description.map((description, indice) => (
+                      <span key={indice}>{description.text}</span>
+                    ))}
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <input
+              type="number"
+              min="1"
+              max={product.data.stock}
+              defaultValue={cantidad}
+              key={cantidad}
+              onChange={(e) => {
+                setCantidad(e.target.value);
+              }}
+              disabled={product.data.stock === 0}
+              className="form-input"
+            ></input>
+            <button
+              className="form-input-button"
+              onClick={() => {
+                addProductToCart({ producto: product, cantidad: cantidad });
+              }}
+              disabled={product.data.stock === 0}
+            >
+              Add to cart!
+            </button>
+          </div>
+          <table className="details-table">
+            <thead>
+              <tr>
+                <th colSpan="2">Specs</th>
+              </tr>
+            </thead>
+            <tbody>
+              {product.data.specs.map((spec, indice) => (
+                <tr key={indice}>
+                  <td>
+                    <b>{spec.spec_name}: </b>
+                  </td>
+                  <td>{spec.spec_value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProductDetail;
---
+++ src/tests/FeaturedSliders.test.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  render,
+  screen,
+  cleanup,
+  waitFor,
+  getByText,
+} from "@testing-library/react";
+import HomePageComponent from "../components/HomePageComponent";
+import { BrowserRouter as Router } from "react-router-dom";
+//import App from "../App";
+
+afterEach(cleanup);
+const sliderTexts = [
+  "AMAZING FINISHES - BEDROOM",
+  "A GREAT STYLE - LIVING ROOM",
+  "GREAT VERSATILITY - DINING ROOM",
+];
+const carouselTexts = [
+  "Bed & Bath",
+  "Lighting",
+  "Kitchen",
+  "Furniture",
+  "Decorate & Organize",
+];
+const gridTexts = [
+  "Grayton Armchair",
+  "Tallulah Sofa Gray",
+  "Tyler Poly Reclining Leather Armchair",
+  "Thatcher Armchair",
+  "Shay Armchair",
+  "Spear Armchair",
+  "Roscoe Armchair Gray",
+  "Chesterfield Sofa Dark Brown",
+  "Brown Turner Right Arm Armchair",
+  "Balboa Armchair",
+  "Comfort Aquare Arm Sofa",
+  "PB Comfort Roll Arm Sofa",
+  "Big Sur Modular Room",
+  "Pearce Modular Sofa Left Arm",
+  "Farmhouse King Size Bed",
+  "Studded Armchair Irving",
+];
+it("renders slider correctly", async () => {
+  render(
+    <Router>
+      <HomePageComponent />
+    </Router>
+  );
+  await waitFor(() => {
+    const sliderNode = screen.getByTestId("slider");
+    expect(sliderNode.children).toHaveLength(3 * 2);
+    sliderTexts.forEach((sliderText) => {
+      const textNode = screen.getByText(sliderText);
+      expect(textNode).toBeInTheDocument();
+    });
+  });
+});
+it("renders carousel correctly", async () => {
+  render(
+    <Router>
+      <HomePageComponent />
+    </Router>
+  );
+  await waitFor(() => {
+    const carouselNode = screen.getByTestId("carousel");
+    expect(carouselNode.children).toHaveLength(5);
+    carouselTexts.forEach((carouselText) => {
+      const textNode = screen.getByText(carouselText);
+      expect(textNode).toBeInTheDocument();
+    });
+  });
+});
+it("renders grid correctly", async () => {
+  render(
+    <Router>
+      <HomePageComponent />
+    </Router>
+  );
+  await waitFor(() => {
+    const gridNode = screen.getByTestId("grid");
+    expect(gridNode.children).toHaveLength(16);
+    gridTexts.forEach((gridText) => {
+      const textNode = screen.getByText(`${gridText}`, { exact: false });
+      expect(textNode).toBeInTheDocument();
+    });
+  });
+});
+//To be continued
